1de133e6e9e0493bfd49a84abba7c8cad64d1721
Sam <56245928+syzxy@users.noreply.github.com>
2022-10-26 10:51:41 +1100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { Marquee } from "./Marquee";
+
+export default function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Marquee></Marquee>
+    </div>
+  );
+}
---
+++ src/Marquee.tsx
@@ -0,0 +1,3 @@
+export function Marquee() {
+  return <div>Marquee!</div>
+}
